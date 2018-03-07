09c3ae51e8fb5222ffde550b42548da3c4b2f931
zerolhao <zerolhao@gmail.com>
2018-03-07 14:56:42 +0800

    add A34-AJAX

---
+++ A34-AJAX/main.js
@@ -0,0 +1,23 @@
+myButton.addEventListener('click',function(){
+  let request = new XMLHttpRequest() // 创建
+  request.open('GET', '/xxx', true) // 配置
+  request.send() // 发送
+  request.onreadystatechange = ()=>{
+    if(request.readyState === 4){
+      // 请求响应都完毕了
+      console.log('request.readyState', request.readyState)
+      
+      if(request.status >= 200 && request.status < 300){
+        // 请求成功
+        console.log('request.status', request.status)
+        
+        let string = request.responseText
+        let obj = window.JSON.parse(string)
+        console.log('typeof request.reponseText', typeof request.responseText)
+        console.log('typeof JSON.parse(request.responseText)', typeof obj)
+        console.log('request.responseText', string)
+        console.log('JSON.parse(request.responseText)', obj)
+      }
+    }
+  }
+})
